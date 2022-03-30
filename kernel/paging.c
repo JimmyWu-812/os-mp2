aa19f0f80a94e87ce5c08f0dc5e5204bb9b3c002
Jimmy <jimmy881122naruto@gmail.com>
2022-03-31 02:00:15 +0800

    4.2.2

--- kernel/paging.c
+++ kernel/paging.c
@@ -10,8 +10,22 @@
 /* Page fault handler */
 int handle_pgfault() {
   /* Find the address that caused the fault */
-  /* uint64 va = r_stval(); */
+  uint64 va = r_stval();
 
   /* TODO */
-  panic("not implemented yet\n");
+  struct proc *p = myproc();
+  char *pa = kalloc();
+
+  va = PGROUNDDOWN(va);
+  if(pa == 0){
+    return -1;
+  }
+  memset(pa, 0, PGSIZE);
+  if(mappages(p->pagetable, va, PGSIZE, (uint64)pa, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+    kfree(pa);
+    return -1;
+  }
+  return 0;
+
+  // panic("not implemented yet\n");
 }
